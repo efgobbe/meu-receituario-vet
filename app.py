9dd5d44f2702d27b0249c16eef9b007f4bc62b47
efgobbe <eliesergobbe@gmail.com>
2026-03-18 11:37:35 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from fpdf import FPDF
 from datetime import date
 import os
 
-# --- DADOS FIXOS DO DR. ELIÉSER ---
+# --- DADOS FIXOS DO PROFISSIONAL ---
 NOME_VET = "Dr. Eliéser Ferreira Gobbe"
 TITULO = "Médico Veterinário"
 REGISTRO = "CRMV-SC 2754"
@@ -19,7 +19,11 @@ with st.form("form_receita"):
     col1, col2 = st.columns(2)
     with col1:
         paciente = st.text_input("Nome do Animal")
-        especie = st.text_input("Espécie")
+        # --- CAMPO DE ESPÉCIE COM OPÇÕES ---
+        especie = st.selectbox(
+            "Espécie", 
+            ["Canina", "Felina", "Equina", "Bovina", "Ovina", "Caprina", "Suína", "Ave", "Outra"]
+        )
     with col2:
         proprietario = st.text_input("Proprietário/Tutor")
         data_hoje = date.today().strftime("%d/%m/%Y")
@@ -72,21 +76,21 @@ if btn_gerar:
     pdf.set_font("Arial", '', 11)
     pdf.multi_cell(0, 7, txt=prescricao)
     
-    # Assinatura Veterinário - COM ESPAÇO ENTRE LINHA E NOME
+    # Assinatura Veterinário
     pdf.ln(20)
     pdf.cell(0, 0, txt="__________________________________________", ln=True, align='C')
-    pdf.ln(5)  # Espaço de 5mm entre a linha e o seu nome
+    pdf.ln(5)
     pdf.set_font("Arial", 'B', 10)
     pdf.cell(0, 7, txt=NOME_VET, ln=True, align='C')
     pdf.set_font("Arial", '', 9)
     pdf.cell(0, 5, txt=f"{TITULO} - {REGISTRO}", ln=True, align='C')
     pdf.cell(0, 5, txt=f"Data: {data_hoje}", ln=True, align='C')
 
-    # Rodapé Técnico em Colunas
+    # Rodapé Técnico
     pdf.ln(10)
     y_rodape = pdf.get_y()
     
-    # Coluna Esquerda: Comprador
+    # Comprador
     pdf.set_xy(10, y_rodape)
     pdf.set_font("Arial", 'B', 9)
     pdf.cell(95, 6, txt="Identificação do Comprador", ln=False, align='C')
@@ -100,7 +104,7 @@ if btn_gerar:
     pdf.set_x(10)
     pdf.cell(95, 5, txt="Cidade: ___________ UF: ___ Tel: ________", ln=True, align='C')
     
-    # Coluna Direita: Fornecedor
+    # Fornecedor
     pdf.set_xy(105, y_rodape)
     pdf.set_font("Arial", 'B', 9)
     pdf.cell(95, 6, txt="Identificação do Fornecedor", ln=False, align='C')
